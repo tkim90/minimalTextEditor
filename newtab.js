a3b543e39ff2161498a486a00799bcb175184b9a
Tae Kim <taehnkim@gmail.com>
2019-07-04 00:30:27 +0900

    Changed formatting, added to-dos

--- newtab.js
+++ newtab.js
@@ -1,56 +1,58 @@
-// To-Do
-// don't fade out after mouse is moved
-// multiple tabs
-// reduce indent space from 4 to 2
-// make screen responsive to larger monitors
-// undo function
+/*
+TO DO
+  sound mute fails to save in new tab
+  sound mute + dark mode ... fails to save mute and dark mode
+  change tabs to spaces
+  reduce indent space from 4 to 2 (tab in new line )
+  don't fade out after mouse is moved
+  make screen responsive to larger monitors
+  make textBox editable thru drag
+  undo function
+  multiple tabs
 
-// DONE
-// fullscreen mode
-// add save button
-
+DONE
+  auto-set download filename.txt to today's date + time
+*/
 
 let editor = {
 
-
   init: function () {
-    // initialize typing sound
-    let sound = new Howl({
-      src: ['assets/t2.wav']
-    });
-
-    // play typing sounds
-    $('.canvas').on('keyup', function(e) {
-      if ( (e.which <= 90 && e.which >= 48) || (e.which <=222 && e.which >= 219) || (e.which <= 192 && e.which >= 186) ) {
+    let sound = new Howl({ src: ['assets/t2.wav'] });
+    
+    // play sound while typing
+    $('.textBox').on('keydown', function(e) {
+      if ( (e.which <= 90 && e.which >= 48) 
+      || (e.which <=222 && e.which >= 219)
+      || (e.which <= 192 && e.which >= 186) ) {
+        console.log('Playing sound!')
         sound.play();
       }
     });
 
-    // hide stuff when you start typing
-    $('.canvas').on('keydown', hidePanel);
+    // hide panel when I start typing
+    $('.textBox').on('keydown', hidePanel);
 
-    // unhide stuff when you move mouse
-    $('.canvas').on('mousemove', function() {
-      $(this)
-        .css('border','1px dashed #999')
+    // hide panel on mouse move
+    $('.textBox').on('mousemove', function() {
+      $(this).css('border','1px dashed #999');
       $('.btns').show();
     });
 
-    // toggle mute button
+    // toggle typing button
     $('#vol-btn').on('click', function() {
       if ($(this).text() == '+') {
         $(this).text('-');
-        sound.mute(true)
+        sound.mute(true);
       } else {
-        $(this).text('+')
-        sound.mute(false)
+        $(this).text('+');
+        sound.mute(false);
       }
     });
 
-    // night mode button
+    // toggle dark mode
     $('.night-btn').on('click', function () {
       $("body").toggleClass('night');
-      $("textarea").toggleClass('canvas-dark');
+      $("textarea").toggleClass('textBox-dark');
     });
 
     // save button
@@ -64,56 +66,55 @@ let editor = {
     });
 
     // tab to indent
-    $(document).delegate('#canvas', 'keydown', function(e) {
-      let keyCode = e.keyCode || e.which;
-
-      if (keyCode == 9) {
+    $('#textBox').on('keydown', function(e) {
+      if (e.keyCode == 9 || e.which == 9) {
         e.preventDefault();
-        let start = this.selectionStart;
-        let end = this.selectionEnd;
-
-        // set textarea value to: text before caret + tab + text after caret
-        $(this).val($(this).val().substring(0, start)
-                    + "\t"
-                    + $(this).val().substring(end));
-
-        // put caret at right position again
-        this.selectionStart =
-        this.selectionEnd = start + 1;
+        var s = this.selectionStart;
+        $(this).val(function(i, v) {
+          return v.substring(0, s) + "\t" + v.substring(this.selectionEnd)
+        });
+        this.selectionEnd = s + 1;
       }
     });
 
+    // ---Functions--- //
+    function getDate() {
+      var d = new Date();
+      var month = d.getMonth()+1;
+      var day = d.getDate();
+      var hour = d.getHours();
+      var min = d.getMinutes();
 
-    ///////////////////////
-    // === Functions === //
-    ///////////////////////
+      return output = d.getFullYear() + '-' +
+        (month<10 ? '0' : '') + month + '-' +
+        (day<10 ? '0' : '') + day + '-' +
+        hour + '-' + min;
+    }
+
     function saveFile () {
-    $('#prompt').addClass("active");
-    $('#prompt #fileName')
-      .val("filename.txt")
-      .focus().select();
-    $('#prompt #fileSave').click(function(){
-      makeTextFile();
-      $('#prompt').removeClass("active");
-    });
+      $('#prompt').addClass("active");
+      $('#prompt #fileName')
+        .val(getDate() + ".txt")
+        .focus().select();
+      $('#prompt #fileSave').click(function(){
+        makeTextFile();
+        $('#prompt').removeClass("active");
+      });
       $('#prompt #fileCancel').click(function(){
         $('#prompt').removeClass("active");
       });
     }
 
-
     function makeTextFile () {
       let link = document.createElement("a");
       link.download = $('#prompt #fileName').val();
-      if(link.download =="")
-        link.download = "document.txt";
-      link.href = "data:text/plain,"+data.textEncode($('.canvas').val());
+      if(link.download =="") { link.download = "document.txt"; }
+      link.href = "data:text/plain,"+data.textEncode($('.textBox').val());
       link.click();
     }
 
-    // function to hide stuff
     function hidePanel(e) {
-        $('.canvas, .btns')
+        $('.textBox, .btns')
         .delay(3000)
         .queue(function(next) {
           $(this).css('border','none');
@@ -128,15 +129,10 @@ let editor = {
       } else {
         document.documentElement.webkitRequestFullScreen();
       }
-}
-
+    }
 
   } // end init function
-  
-
 } // end editor object
-
-
 
 $(document).ready(function () {
   editor.init();
